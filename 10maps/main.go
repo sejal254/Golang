5087c9a272f3b944642542d26390f5f2dfd9fc1c
Sejal Nayak <65972138+sejal254@users.noreply.github.com>
2023-03-24 15:59:36 +0530

    Update main.go

--- 10maps/main.go
+++ 10maps/main.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	//maps also called hashtabel
-	fmt.Println("welcome to Maps")
-	//use make to create slices and maps
-	languages :=make(map[string]string)
-	languages["JS"]="javascript"
-	languages["py"]="python"
-	languages["ry"]="Ruby"
 
-	fmt.Println("List of languages: ",languages)
-	fmt.Println("Js is for",languages["JS"])
+// func main() {
+// 	//maps also called hashtabel
+// 	fmt.Println("welcome to Maps")
+// 	//use make to create slices and maps
+// 	languages :=make(map[string]string)
+// 	languages["JS"]="javascript"
+// 	languages["py"]="python"
+// 	languages["ry"]="Ruby"
+
+// 	fmt.Println("List of languages: ",languages)
+// 	fmt.Println("Js is for",languages["JS"])
+// }
+
+type vertex struct{
+	l, h float64
+}
+
+var m map[string]vertex
+
+func main(){
+	m=make(map[string]vertex)
+	m["area"]=vertex{
+		23.34, 76.34,
+	}
+	fmt.Println(m["area"])
 }
 
